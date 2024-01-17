af955a7a3b7f4841e0154fc34c9325cd578aee5e
Iryna <iryna.berezkina@gmail.com>
2024-01-17 22:00:12 +0100

    First commit to repo.

---
+++ src/test/java/pl/testelka/fakestore/BaseTest.java
@@ -0,0 +1,4 @@
+package pl.testelka.fakestore;
+
+public class BaseTest {
+}
---
+++ src/test/java/pl/testelka/fakestore/LoginTest.java
@@ -0,0 +1,10 @@
+package pl.testelka.fakestore;
+
+import org.junit.jupiter.api.Test;
+
+public class LoginTest {
+    @Test
+    public void test(){
+        System.out.println("Hello world!");
+    }
+}
