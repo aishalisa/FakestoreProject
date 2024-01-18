dda62b4a80335509809365ebeb5ec6dfd1ce4159
Iryna <iryna.berezkina@gmail.com>
2024-01-18 23:29:50 +0100

    driver setup

--- src/test/java/pl/testelka/fakestore/BaseTest.java
+++ src/test/java/pl/testelka/fakestore/BaseTest.java
@@ -1,4 +1,37 @@
 package pl.testelka.fakestore;
 
-public class BaseTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+
+abstract public class BaseTest {
+ WebDriver driver;
+ Wait<WebDriver> wait;
+    @BeforeAll
+    static public void setupChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    void setup(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless==new");
+        driver = new ChromeDriver(options);
+        wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofMillis(300))
+                .ignoring(ElementNotInteractableException.class);
+    }
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
 }
