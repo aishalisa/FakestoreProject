292627e860d4e4405ed1a507df857aadd50b62bf
Iryna <iryna.berezkina@gmail.com>
2024-01-23 23:13:49 +0100

    added 2 more tests to LoginTest

--- src/main/java/pl/testelka/fakestore/LoginPage.java
+++ src/main/java/pl/testelka/fakestore/LoginPage.java
@@ -4,6 +4,7 @@ import helpers.CredentialsReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -22,6 +23,8 @@ public class LoginPage extends BasePage {
     WebElement loginButton;
     @FindBy(css = "h1.entry-title")
     WebElement title;
+    @FindBy(className = "is-error")
+    WebElement errorPopup;
 
     public String enterCredentials() {
         CredentialsReader credentials = new CredentialsReader();
@@ -31,5 +34,17 @@ public class LoginPage extends BasePage {
         passwordField.sendKeys(userPassword);
         loginButton.click();
         return title.getText();
+    }
+
+    public Boolean enterWrongCredentials() {
+        emailField.sendKeys("xxxxx");
+        passwordField.sendKeys("xxx");
+        loginButton.click();
+        return errorPopup.isDisplayed();
+    }
+
+    public Boolean loginWithoutCredentials() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+        return errorPopup.isDisplayed();
     }
 }
--- src/test/java/pl/testelka/fakestore/LoginTest.java
+++ src/test/java/pl/testelka/fakestore/LoginTest.java
@@ -5,11 +5,28 @@ import org.junit.jupiter.api.Test;
 
 public class LoginTest extends BaseTest {
     @Test
-    public void loginTest() {
+    public void loginValidPath() {
         HomePage homePage = new HomePage(driver);
         homePage.goToLoginPage();
         LoginPage loginPage = new LoginPage(driver);
         String title = loginPage.enterCredentials();
         Assertions.assertEquals("Moje konto", title, "You failed to login.");
+    }
+
+    @Test
+    public void loginWrongCredentials() {
+        HomePage homePage = new HomePage(driver);
+        homePage.goToLoginPage();
+        LoginPage loginPage = new LoginPage(driver);
+        Boolean errorPopupIsVisible = loginPage.enterWrongCredentials();
+        Assertions.assertTrue(errorPopupIsVisible, "No error message visible, maybe your credentials are OK?");
+    }
+    @Test
+    public void loginWithoutCredentials(){
+        HomePage homePage = new HomePage(driver);
+        homePage.goToLoginPage();
+        LoginPage loginPage = new LoginPage(driver);
+        Boolean errorPopupIsVisible = loginPage.loginWithoutCredentials();
+        Assertions.assertTrue(errorPopupIsVisible);
     }
 }
