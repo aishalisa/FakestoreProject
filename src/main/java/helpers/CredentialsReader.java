682512271a69725ddde04dd551946f425c5e295e
Iryna <iryna.berezkina@gmail.com>
2024-02-18 18:50:43 +0100

    added assertion checking if an order was completed

--- src/main/java/helpers/CredentialsReader.java
+++ src/main/java/helpers/CredentialsReader.java
@@ -9,6 +9,16 @@ import java.util.Properties;
 public class CredentialsReader {
     private final String userEmail;
     private final String userPassword;
+    private final String surname;
+    private final String name;
+    private final String street;
+    private final String postCode;
+    private final String city;
+    private final String phone;
+    private final String cardNumber;
+    private final String cvc;
+    private final String validDate;
+
 
     public CredentialsReader() {
         String configPath = "src/test/resources/credentials.properties";
@@ -28,6 +38,15 @@ public class CredentialsReader {
         }
         userEmail = properties.getProperty("userEmail");
         userPassword = properties.getProperty("userPassword");
+        name = properties.getProperty("name");
+        surname = properties.getProperty("surname");
+        street = properties.getProperty("street");
+        postCode = properties.getProperty("postCode");
+        phone = properties.getProperty("phone");
+        cardNumber = properties.getProperty("cardNumber");
+        cvc = properties.getProperty("cvc");
+        validDate = properties.getProperty("validDate");
+        city = properties.getProperty("city");
     }
     public String getUserEmail() {
         return userEmail;
@@ -35,5 +54,41 @@ public class CredentialsReader {
 
     public String getUserPassword() {
         return userPassword;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getCvc() {
+        return cvc;
+    }
+
+    public String getValidDate() {
+        return validDate;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
